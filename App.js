263af1045f1d1b75efecb8af1254ff3f001ac4db
Android Studio <devandroid@ort>
2022-09-28 10:43:16 -0300

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Clock from 'react-live-clock';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>HORA ACTUAL / TEMPERATURA</Text>
+      <Clock format={'HH:mm:ssa'} ticking={true} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function ContactsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>CONTACTOS Y CONFIGURACION DE NUMERO DE EMERGENCIA</Text>
+    </View>
+  );
+}
+
+function AboutUsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>IDENTIFICACION DE CADA APLICACIÓN</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+            <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Contacts') {
+              iconName = focused ? 'people' : 'people-outline';
+            }  else if (route.name === 'AboutUs') {
+              iconName = focused ? 'information-circle' : 'information-circle-outline';
+            } 
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Contacts" component={ContactsScreen} />
+        <Tab.Screen name="AboutUs" component={AboutUsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
