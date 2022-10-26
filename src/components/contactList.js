8bad73f72bac779f8f6745e568ade09ad61ce563
Android Studio <devandroid@ort>
2022-10-26 09:56:14 -0300

    Fix contacts & homepage

--- src/components/contactList.js
+++ src/components/contactList.js
@@ -1,33 +1,13 @@
 import { AlphabetList } from "react-native-section-alphabet-list";
 import React from 'react';
-import contactsData from '../helpers/contactsData';
+import { contactsData } from '../helpers/contactsData';
 import { Text, View } from "react-native";
 import styles from '../styles/contactList';
 
 export default async function ContactList() {
     data = await contactsData()
+    console.log(data)
     return (
-        <AlphabetList
-        data={data}
-        indexLetterStyle={{ 
-            color: 'black', 
-            fontSize: 15,
-        }}
-        renderCustomListHeader={() => (
-            <View style={styles.listHeaderContainer}>
-                <Text style={styles.listHeaderLabel}>{data.length} contactos</Text>
-            </View>
-        )}
-        renderCustomItem={(item) => (
-            <View style={styles.listItemContainer}>
-                <Text style={styles.listItemLabel}>{item.value} ({item.phone})</Text>
-            </View>
-        )}
-        renderCustomSectionHeader={(section) => (
-            <View style={styles.sectionHeaderContainer}>
-                <Text style={styles.sectionHeaderLabel}>{section.title}</Text>
-            </View>
-        )}
-        />
+        <Text>Hola</Text>
     )
 }
--- src/helpers/contactsData.js
+++ src/helpers/contactsData.js
@@ -1,17 +1,18 @@
 import * as Contacts from 'expo-contacts';
+import { useEffect } from 'react';
 
 export default async function contactsData() {
-    const [conctacts, setContacts] = useState([])
 
-    const { status } = await Contacts.requestPermissionsAsync();
-        
-    if (status === 'granted') {
-        const { data } = await Contacts.getContactsAsync({
-            fields: [Contacts.Fields.Emails, Contacts.Fields.PhoneNumbers],
-        });
+    useEffect(() => {
+        (async () => {
+          const { status } = await Contacts.requestPermissionsAsync();
+          if (status === 'granted') {
+            const { data } = await Contacts.getContactsAsync({
+              fields: [Contacts.Fields.Emails, Contacts.Fields.PhoneNumbers],
+            });
+            console.log(data)
+          }
+        })();
+    }, []);
 
-        setContacts(data)
-    }
-
-    return data;
 }
